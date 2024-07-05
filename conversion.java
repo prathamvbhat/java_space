1105666abeb4139d7b57a8121116a4abf63fdb25
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-07-05 06:57:33 +0530

    added conversion.java

---
+++ conversion.java
@@ -0,0 +1,11 @@
+public class conversion {
+    public static void main(String[] args){
+        int num1 = 5;
+        double num2 = num1;
+        System.out.println("num2: " +num2);
+
+        
+    }
+
+
+}
