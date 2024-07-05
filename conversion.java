3408ad7193aa5616c155f871509fc62ea95f4e96
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-07-05 20:56:49 +0530

    updated conversion

--- conversion.java
+++ conversion.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class conversion {
     public static void main(String[] args){
         // int num1 = 5;
@@ -5,8 +7,17 @@ public class conversion {
         // System.out.println("num2: " +num2);
 
         // Operators
-        int num1 = 12;
-        int num2 = 6;
+        // int num1 = 12;
+        // int num2 = 6;
+        int num1, num2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Number 1: ");
+        num1 = scanner.nextInt();
+
+        System.out.println("Enter Number 2: ");
+        num2 = scanner.nextInt();
+
         //addition
         System.out.println(num1 + num2);
         //substraction
