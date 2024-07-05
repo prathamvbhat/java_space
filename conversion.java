2638625c03bbbdf6550656c94b55f1a796d9835a
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-07-05 20:49:32 +0530

    added conversion.java

--- conversion.java
+++ conversion.java
@@ -1,10 +1,22 @@
 public class conversion {
     public static void main(String[] args){
-        int num1 = 5;
-        double num2 = num1;
-        System.out.println("num2: " +num2);
+        // int num1 = 5;
+        // double num2 = num1;
+        // System.out.println("num2: " +num2);
 
-        
+        // Operators
+        int num1 = 12;
+        int num2 = 6;
+        //addition
+        System.out.println(num1 + num2);
+        //substraction
+        System.out.println(num1 - num2);
+        //multiplication
+        System.out.println(num1 * num2);
+        //division
+        System.out.println(num1 / num2);
+        //modulo
+        System.out.println(num1 % num2);
     }
 
 
