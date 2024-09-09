36b78d42b2cf510a11cf908c1e39776af34b7846
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-09-09 20:32:15 +0530

    learn: nested switch

--- Nested_switch.java
+++ Nested_switch.java
@@ -2,35 +2,34 @@ import java.util.Scanner;
 
 public class Nested_switch {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the Employee ID: ");
-       int empId = sc.nextInt();
-       System.out.println("Enter the Department: ");
-       String dept = sc.next();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Employee ID: ");
+        int empId = sc.nextInt();
+        System.out.println("Enter the Department: ");
+        String dept = sc.next();
 
-    switch(empID){
-        case 1:
-            System.out.println("Pratham V Bhat");
-            break;
-        case 2:
-            System.out.println("Omkar Bhat");
-            break;
-        case 3:
-            System.out.println("Employee Number 3");
-            switch(dept) {
-                case "IT":
-                    System.out.println("IT Departmant");
-                    break;
-                case "MG":
-                    System.out.println("Management Department");
-                    break;
-                default:
-                    System.out.println("Enter a valid Department");
-            }
-            break;
-        default:
-            System.out.println("Enter a valid Employee ID");
-       }
-
+        switch(empId){
+            case 0:
+                System.out.println("Pratham V Bhat");
+                break;
+            case 1:
+                System.out.println("Omkar Bhat");
+                break;
+            case 2:
+                System.out.println("Employee Number 2");
+                switch(dept) {
+                    case "IT":
+                        System.out.println("IT Departmant");
+                        break;
+                    case "MG":
+                        System.out.println("Management Department");
+                        break;
+                    default:
+                        System.out.println("Enter a valid Department");
+                }
+                break;
+            default:
+                System.out.println("Enter a valid Employee ID");
+        }
     }
 }
