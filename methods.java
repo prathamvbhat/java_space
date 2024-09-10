f2c8502e89250be1de9742c4a7451458b08081e7
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-09-10 11:28:08 +0530

    learn: methods/function in java

--- methods.java
+++ methods.java
@@ -2,16 +2,28 @@
 import java.util.Scanner;
 public class methods {
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println("The sum is: "+ans);
     }
 
-    static void sum(){
+    // static void sum(){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter 1st Number: ");
+    //     int num1 = sc.nextInt();
+    //     System.out.println("Enter 2nd Number: ");
+    //     int num2 = sc.nextInt();
+    //     int sum = num1 + num2;
+    //     System.out.println("Sum of two numbers: "+sum);
+    // }
+    // OR
+
+    static int sum2(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1st Number: ");
         int num1 = sc.nextInt();
         System.out.println("Enter 2nd Number: ");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
-        System.out.println("Sum of two numbers: "+sum);
+        return sum;
     }
 }
