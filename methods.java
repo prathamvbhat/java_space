97305aac5952a3f8f5b1554ecfdda303677b5c80
prathamvbhat <21a36.pratham@sjec.ac.in>
2024-09-10 11:37:55 +0530

    learn: methods/function in java

--- methods.java
+++ methods.java
@@ -2,8 +2,14 @@
 import java.util.Scanner;
 public class methods {
     public static void main(String[] args) {
-        int ans = sum2();
-        System.out.println("The sum is: "+ans);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter 2nd Number: ");
+        int num2 = sc.nextInt();
+        // int ans = sum2(num1, num2);
+        sum2(num1, num2);
+        // System.out.println("The sum is: "+ans);
     }
 
     // static void sum(){
@@ -17,13 +23,26 @@ public class methods {
     // }
     // OR
 
-    static int sum2(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1st Number: ");
-        int num1 = sc.nextInt();
-        System.out.println("Enter 2nd Number: ");
-        int num2 = sc.nextInt();
+    // static int sum2(){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter 1st Number: ");
+    //     int num1 = sc.nextInt();
+    //     System.out.println("Enter 2nd Number: ");
+    //     int num2 = sc.nextInt();
+    //     int sum = num1 + num2;
+    //     return sum;
+    // }
+
+    //pass the value of the numbers while calling thr function and return the value
+    // static int sum2(int num1, int num2){
+    //     int sum = num1 + num2;
+    //     return sum;
+    // }
+
+    //OR pass the value of the numbers while calling the function and print the value
+
+    static void sum2(int num1, int num2){
         int sum = num1 + num2;
-        return sum;
+        System.out.println("Sum of two numbers: "+sum);
     }
 }
